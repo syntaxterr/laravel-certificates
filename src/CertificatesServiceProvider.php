01e59639d7add66f919852acffb296b0cf38390c
Dennis Nielsen <dennis@syntaxterror.dk>
2024-10-17 15:35:48 +0200

    Added service provider

---
+++ src/CertificatesServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Syntaxterr\Laravel\Certificates;
+
+use Illuminate\Support\ServiceProvider;
+
+class CertificatesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
